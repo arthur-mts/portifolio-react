b596ec337162062de2118ec887991348cd6f1ada
punisher077 <punisher077@github.com>
2020-01-10 03:49:02 -0200

    feita animaçao  das techs

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,22 @@ import React from 'react';
 // import { Container } from './styles';
 import './style.css';
 export default function About() {
+
+  //const [colorProp, setColorProp] = useEffect();
+  //const [percentProp, setPercentProp] = useEffect();
+  const mapTechs = new Map();
+  const techs = ["JavaScript", "Java","Python","Banco de Dados","HTML","NodeJS","Linux","Git/GitHub"];
+
+  mapTechs.set("NodeJS", {color: "#8CC84C", percent: 60});
+  mapTechs.set("JavaScript", {color: "#F7E018", percent: 60});
+  mapTechs.set("Python",{color: "#3775A8", percent: 70});
+  mapTechs.set("Java",{color: "#E82D2F", percent: 80});
+  mapTechs.set("HTML",{color: "#EA6228", percent: 50});
+  mapTechs.set("Linux",{color: "#E53B00", percent: 90});
+  mapTechs.set("Banco de Dados",{color: "#32648D", percent: 60});
+  mapTechs.set("Git/GitHub",{color: "#E94D32", percent: 80});
+
+
   return (
     <div className="aboutMe">
       <p>
@@ -11,60 +27,22 @@ export default function About() {
 
       <h2>Habilidades</h2>
       <ul>
-        <li>
-          <div className="bar">
-            <div className="progress"></div>
-          </div>
-          <p>
-            JavaScript
-          </p>
-
-        </li>
-
-        <li>
-          <div className="bar">
-            <div className="progress"></div>
-          </div>
-          <p>
-            JavaScript
-          </p>
-        </li>
-
-        <li>
-          <div className="bar">
-            <div className="progress"></div>
-          </div>
-          <p>
-            JavaScript
-          </p>
-        </li>
-
-        <li>
-          <div className="bar">
-            <div className="progress"></div>
-          </div>
-          <p>
-            JavaScript
-          </p>
-        </li>
-
-        <li>
-          <div className="bar">
-            <div className="progress"></div>
-          </div>
-          <p>
-            JavaScript
-          </p>
-        </li>
-
-        <li>
-          <div className="bar">
-            <div className="progress"></div>
-          </div>
-          <p>
-            JavaScript
-          </p>
-        </li>
+        {techs.map((tech,i) => {
+          const {color, percent} = mapTechs.get(tech);
+          console.log(color, percent);
+          //setColorProp(color);
+          //setPercentProp(percent);
+          return(
+           <li key={i}>
+           <div className="bar">
+             <div className="progress" style={{backgroundColor: color, width: `${percent}%`}}></div>
+           </div>
+           <p>
+             {tech}
+           </p>
+         </li>
+          )
+        })}
       </ul>
     </div>
   );
