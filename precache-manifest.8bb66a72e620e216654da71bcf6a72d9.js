f67b2b4ef5ce35ab8e03353b49ebdf1984d221a0
punisher077 <punisher077@github.com>
2020-01-10 04:10:28 -0200

    Updates

---
+++ precache-manifest.8bb66a72e620e216654da71bcf6a72d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815d4b1b637eb98e600f53830d959ec2",
+    "url": "/portifolio-react/index.html"
+  },
+  {
+    "revision": "2a5f56274aa7a1fad74a",
+    "url": "/portifolio-react/static/css/main.55a9c8e6.chunk.css"
+  },
+  {
+    "revision": "98134c312bc96a98a2d3",
+    "url": "/portifolio-react/static/js/2.7fc98a8b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portifolio-react/static/js/2.7fc98a8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a5f56274aa7a1fad74a",
+    "url": "/portifolio-react/static/js/main.ea00d62f.chunk.js"
+  },
+  {
+    "revision": "a6b9a5aa72aeb4ca38ae",
+    "url": "/portifolio-react/static/js/runtime-main.829dddc7.js"
+  },
+  {
+    "revision": "7bc6f199384965617b6a3faabec23029",
+    "url": "/portifolio-react/static/media/ifpb-vertical.7bc6f199.png"
+  },
+  {
+    "revision": "72d831089f8bba1f28d9487c86b3c1b6",
+    "url": "/portifolio-react/static/media/profile_photo.72d83108.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aaa57066a2baced0ea1a08e6ea713282.js"
+  "/portifolio-react/precache-manifest.8bb66a72e620e216654da71bcf6a72d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portifolio-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
